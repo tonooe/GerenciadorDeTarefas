f84b4e84d50ca6314e68b3fa574f30e4b03d8d42
Manuelli <manuelliliveira7@gmail.com>
2025-01-30 10:35:01 -0300

    adicionei um comentario no codigo

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.exibirMenu(); // inicia o menu interativo
+        // comentario para salvar com algo diferente do antigo codigo ;)
     }
 }
